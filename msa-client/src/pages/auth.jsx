813cda603b8aa4d3fe154b4406ab5d92b9d47ccf
Ghost Developer <ghost@proton.me>
2026-03-14 11:22:14 +0000

    wp changed

--- msa-client/src/pages/auth.jsx
+++ msa-client/src/pages/auth.jsx
@@ -29,7 +29,7 @@ const getDomainConfig = () => {
         return {
             variant: 'msa3',
             storageKey: 'MSA3_user',
-            whatsapp: '917357984291',
+            whatsapp: '917891202468',
             telegram: 'hackerbabaji1',
             youtube: 'https://www.youtube.com/watch?v=-HdcugtTRN4' // Default if not provided
         };
